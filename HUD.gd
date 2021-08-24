1c58ebb3b29c02d223225d3df4298960e4b098ff
Krodha <lucascrucesfernandez@gmail.com>
2021-08-24 23:28:34 +0200

    Perfeccionamiento de mini menu
    Simplemente le hice retoques de sonido y conexion de botones con escenas

--- HUD.gd
+++ HUD.gd
@@ -13,13 +13,14 @@ func _on_Player_tree_exiting():
 
 func _on_Escape_pressed():
 	$MiniMenu.visible = true
-	
-
+	get_tree().call_deferred("stop_scene","res://Level.tscn")
 
 func _on_menu_pressed():
 	get_tree().call_deferred("change_scene","res://Menú.tscn")
+	$Click.play()
 
 
 
 func _on_reanudar_pressed():
 	get_tree().call_deferred("reload_current_scene")
+	$Click.play()
