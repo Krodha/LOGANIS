75d7b5f2c0a02c195e44d2d2ec611fe49289ec2c
Krodha <lucascrucesfernandez@gmail.com>
2021-12-16 21:01:44 +0100

    Arreglé un pequeño bug
    Habia un bug que no detectaba los pinchos como algo que te matara

--- Player.gd
+++ Player.gd
@@ -62,7 +62,7 @@ func _is_on_floor(delta):
 
 
 func _on_Area2D_body_entered(body):
-	if body is StaticBody2D:
+	if body.is_in_group("Obstaculo"):
 		emit_signal("perder")
 	if body.is_in_group("fin"):
 		get_tree().call_deferred("change_scene", "res://Menú.tscn")
