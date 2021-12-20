1a7004916d5133a7aa9a0c9313d3053a1efa9ef5
Krodha <lucascrucesfernandez@gmail.com>
2021-12-20 20:18:51 +0100

    Borré escenas que sobraban
    Borré las escenas de la Cruz y el Skeletillo que sobraban

--- Cruz.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody2D
-
-
-func _on_AreaCruz_body_entered(body):
-	if body is KinematicBody2D:
-		emit_signal("perder")
--- Skeletillo.gd
+++
@@ -1,5 +0,0 @@
-extends StaticBody2D
-
-func _on_AreaSkeleto_body_entered(body):
-	if body is KinematicBody2D:
-		emit_signal("perder")
