31ad95e70113dccc89c2eb593f7952e3951579be
Krodha <lucascrucesfernandez@gmail.com>
2021-12-21 23:04:53 +0100

    Autopausa
    Ahora cuando pierdes se para de mover al parallax layer y hay un timer para reiniciar. Tambien se para el parallax layer cuando le das al menú

--- Level.gd
+++ Level.gd
@@ -1,18 +1,20 @@
 extends Node
 
-
+onready var parallax_mov : bool = true
 
 func _ready():
 	$BGmusic.play()
 
 func _process(delta):
-	get_node("FONDO 3").scroll_base_offset += Vector2(-1,0) * 16 * delta
-	get_node("FONDO 2").scroll_base_offset += Vector2(-2,0) * 48 * delta
-	get_node("FONDO 1").scroll_base_offset += Vector2(-3,0) * 96 * delta
-	get_node("CAMINO").scroll_base_offset += Vector2(-4,0) * 100 * delta
+	if parallax_mov:
+		get_node("FONDO 3").scroll_base_offset += Vector2(-1,0) * 16 * delta
+		get_node("FONDO 2").scroll_base_offset += Vector2(-2,0) * 48 * delta
+		get_node("FONDO 1").scroll_base_offset += Vector2(-3,0) * 96 * delta
+		get_node("CAMINO").scroll_base_offset += Vector2(-4,0) * 100 * delta
 
 func _on_Escape_pressed():
 	$BGmusic.stop()
+	parallax_mov = false
 
 
 """
@@ -31,11 +33,9 @@ func _on_Escape_pressed():
 
 
 func _on_Player_perder():
+	parallax_mov = false
+	yield(get_tree().create_timer(1.0),"timeout")
 	get_tree().call_deferred("reload_current_scene")
-
-
-
-
 
 
 func _on_Fin_body_entered(body):
@@ -44,15 +44,22 @@ func _on_Fin_body_entered(body):
 
 
 func _on_CRUCES_body_entered(body):
+	yield(get_tree().create_timer(1.0),"timeout")
+	parallax_mov = false
 	if body is KinematicBody2D:
 		get_tree().call_deferred("reload_current_scene")
 
 
 func _on_ESQUELETO_body_entered(body):
+	yield(get_tree().create_timer(1.0),"timeout")
+	parallax_mov = false
 	if body is KinematicBody2D:
 		get_tree().call_deferred("reload_current_scene")
 
 
 func _on_ESQUELETO2_body_entered(body):
+	yield(get_tree().create_timer(1.0),"timeout")
+	parallax_mov = false
 	if body is KinematicBody2D:
 		get_tree().call_deferred("reload_current_scene")
+
--- Player.gd
+++ Player.gd
@@ -2,19 +2,12 @@ extends KinematicBody2D
 
 signal perder
 
-
 export (int) var velocidad 
-
 onready var movimiento : Vector2
-
 onready var saltos : int
-
 onready var can_jump : bool = true
-
 const GRAVEDAD = Vector2(0,5)
-
 const JUMP_HEIGHT = -450
-
 const SUELO = Vector2(0,-1)
 
 
@@ -22,11 +15,7 @@ const SUELO = Vector2(0,-1)
 func _process(delta):
 	
 	_is_on_floor(delta)
-	
 	movimiento.x = velocidad
-	
-	
-
 
 
 func _physics_process(delta):
@@ -39,7 +28,6 @@ func _physics_process(delta):
 		can_jump = false
 	
 	if Input.is_action_just_pressed("space") and can_jump:
-	
 		movimiento.y += JUMP_HEIGHT
 		saltos += 1
 
@@ -48,18 +36,10 @@ func _physics_process(delta):
 func _is_on_floor(delta):
 	
 	movimiento += GRAVEDAD * 200 * delta
-	
 	$AnimatedSprite.play()
-	
 	position += movimiento * delta
-	
 	movimiento = move_and_slide(movimiento, SUELO)
 	
-	# Estuve creando la variable de "saltos" para intentar limitar el Nº de saltos a 1 
-
-
-
-
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Obstaculo"):
