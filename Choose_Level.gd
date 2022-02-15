213d38de606475d16e4124541c8da51659c1c8a6
Krodha <lucascrucesfernandez@gmail.com>
2022-02-15 21:39:57 +0100

    Tutorial funcional
    Ahora mismo el tutorial ya está funcional, tan solo está feote, pero lo de tener los textos que te enseñan a jugar, el menú donde escoger si ir a un nivel al tutorial o al menú tambien está listo.
    Ahora le faltan saprites bonitos y un poco de música, a lo mejor.

--- Choose_Level.gd
+++ Choose_Level.gd
@@ -4,3 +4,11 @@ extends Node
 
 func _on_Button_pressed():
 	get_tree().call_deferred("change_scene", "res://Level.tscn")
+
+
+func _on_Atras_pressed():
+	get_tree().call("change_scene", "res://Menú.tscn")
+
+
+func _on_Tutorial_pressed():
+	get_tree().call("change_scene", "res://Tutorial.tscn")
--- Tutorial.gd
+++ Tutorial.gd
@@ -1,6 +1,24 @@
 extends Node
 
+
 var parallax_mov : bool = true
+
+func _ready():
+	if parallax_mov:
+		yield(get_tree().create_timer(3.0), "timeout")
+		$Ola.visible = false
+		$Tuto.visible = true
+		yield(get_tree().create_timer(4.5), "timeout")
+		$Tuto.visible = false
+		$Fin.visible = true
+		yield(get_tree().create_timer(2.0), "timeout")
+		$Fin.visible = false
+		$Dificil.visible = true
+		yield(get_tree().create_timer(2.5), "timeout")
+		$Dificil.visible = false
+		$Carneiro.visible = true
+		yield(get_tree().create_timer(4.0), "timeout")
+		$Carneiro.visible = false
 
 func _process(delta):
 	if parallax_mov:
@@ -9,3 +27,15 @@ func _process(delta):
 
 func _on_Escape_pressed():
 	parallax_mov = false
+
+func _on_CRUCES_body_entered(body):
+	if body is KinematicBody2D:
+		parallax_mov = false
+		yield(get_tree().create_timer(1.0), "timeout")
+		get_tree().call_deferred("reload_current_scene")
+
+
+func _on_Player_perder():
+	parallax_mov = false
+	yield(get_tree().create_timer(1.0), "timeout")
+	get_tree().call_deferred("change_scene", "res://Choose_Level.tscn")
